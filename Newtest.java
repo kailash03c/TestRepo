a0b92686e253bb427a75cdba630a7d405d13ae6f
Kailash03c <Kailash03c@gmail.com>
2024-12-18 18:42:25 +0530

    New branch test

---
+++ Newtest.java
@@ -0,0 +1,7 @@
+class Newtest {
+    public static void main(String[] args) {
+        System.out.println("Hesoyam");
+        System.out.println("Rocket Man");
+        System.out.println("Sanandreas");
+    }
+}
